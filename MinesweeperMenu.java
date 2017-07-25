36b0f826ce492392f13c7d713c5197b823021525
s5016682 <30329913+s5016682@users.noreply.github.com>
2017-07-25 13:16:54 +1000

    Minesweeper v0.01

--- MinesweeperMenu.java
+++ MinesweeperMenu.java
@@ -27,7 +27,7 @@ public class MinesweeperMenu extends JPanel{
   static public void main (String[] args){
     JFrame minesweep = new JFrame("Minesweeper");
     minesweep.getContentPane().add(new MinesweeperMenu());
-    minesweep.setSize(new Dimension(400,400));
+    minesweep.pack();
     minesweep.setVisible(true);
   }
   
@@ -39,14 +39,97 @@ public class MinesweeperMenu extends JPanel{
   
   private class NewGame implements ActionListener{
     public void actionPerformed(ActionEvent e){
-      //JFrame ex = new Mines(30, user);
-      //ex.setVisible(true);
+      JFrame gameSelectMenu = new JFrame("GameOptions");
+      gameSelectMenu.getContentPane().add(new GameSelect());
+      gameSelectMenu.pack();
+      gameSelectMenu.setVisible(true);
     }
   }
   private class NewGameColour implements ActionListener{
     public void actionPerformed(ActionEvent e){
       //JFrame ex = new Mines(40, user);
       //ex.setVisible(true);
+    }
+  }
+  
+  public class GameSelect extends JPanel {
+    private JLabel diff, mode;
+    private JButton start, back;
+    private JRadioButtonMenuItem easy, medium, hard, square, hex;
+    
+    GameSelect(){
+      super();
+      setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+      start = new JButton("Start");
+      start.addActionListener(new StartGame());
+      back = new JButton("Back");
+      back.addActionListener(new GoBack());
+      diff = new JLabel("Difficulty");
+      mode = new JLabel("Game Mode");
+      
+      ButtonGroup diffGroup = new ButtonGroup();
+      JMenu diffMenu = new JMenu("Game Difficulty");
+      easy = new JRadioButtonMenuItem("Easy");
+      easy.addActionListener(new Difficulty());
+      medium = new JRadioButtonMenuItem("Medium");
+      medium.addActionListener(new Difficulty());
+      hard = new JRadioButtonMenuItem("Hard");
+      hard.addActionListener(new Difficulty());
+      diffGroup.add(easy);
+      diffMenu.add(easy);
+      //diffGroup.add(medium);
+      //diffMenu.add(medium);
+      //diffGroup.add(hard);
+      //diffMenu.add(hard);
+      
+      ButtonGroup modeGroup = new ButtonGroup();
+      JMenu modeMenu = new JMenu("Game Mode");
+      square = new JRadioButtonMenuItem("Squares");
+      square.addActionListener(new GameMode());
+      hex = new JRadioButtonMenuItem("Hexagons");
+      hex.addActionListener(new GameMode());
+      modeGroup.add(square);
+      modeMenu.add(square);
+      //modeGroup.add(hex);
+      //modeMenu.add(hex);
+      
+      
+      
+      add(diff);
+      add(diffMenu);
+      add(mode);
+      add(modeMenu);
+      add(start);
+      add(back);
+      setVisible(true);
+    }
+    
+    private class StartGame implements ActionListener{
+      public void actionPerformed(ActionEvent e){
+        //JFrame ex = new Mines(30, user);
+        //ex.setVisible(true);
+      }
+    }
+    
+    private class GoBack implements ActionListener{
+      public void actionPerformed(ActionEvent e){
+        //JFrame ex = new Mines(30, user);
+        //ex.setVisible(true);
+      }
+    }
+    
+    private class Difficulty implements ActionListener{
+      public void actionPerformed(ActionEvent e){
+        //JFrame ex = new Mines(30, user);
+        //ex.setVisible(true);
+      }
+    }
+    
+    private class GameMode implements ActionListener{
+      public void actionPerformed(ActionEvent e){
+        //JFrame ex = new Mines(30, user);
+        //ex.setVisible(true);
+      }
     }
   }
 }
